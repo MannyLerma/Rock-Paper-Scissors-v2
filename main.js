afc8a68ead0a038b7ff6874e626a2fdd90dbd75c
Emmanuel Lerma <Emmanuellerma96@gmail.com>
2022-08-29 17:19:38 -0500

    Make life bar deplete upon round loss

---
+++ main.js
@@ -0,0 +1,100 @@
+const userChoiceDisplay = document.createElement('h3');
+const compChoiceDisplay = document.createElement('h3');
+const resultDisplay = document.createElement('h2');
+
+const gameGrid = document.getElementById('game');
+gameGrid.append(userChoiceDisplay, compChoiceDisplay, resultDisplay);
+
+
+const choices = ['rock', 'paper', 'scissors']
+let userChoice;
+let compChoice;
+
+let playerScore = 0;
+let compScore = 0;
+
+let score = "";
+let result = "";
+const button = document.querySelectorAll('input');
+
+
+// function to create an event handler //
+const handleClick = (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = 'User choice: ' + userChoice
+    generateCompChoice()
+    getResult()
+}
+
+// Generates a random selection for the computer
+const generateCompChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    compChoice = randomChoice
+    compChoiceDisplay.innerHTML = 'Computer choice: ' + compChoice
+}
+
+// Once the game finishes the buttons will be disabled
+function disableButton() {
+    button.forEach(elem => {
+        elem.disabled = true;
+    })
+}
+
+
+// Cycle through the choices //
+for (let i = 0; i < choices.length; i++) {
+    const button = document.querySelector('input')
+    button.id = choices[i]
+    button.innerHTML = choices[i]
+    button.addEventListener('click', handleClick)
+    gameGrid.appendChild(button)
+}
+
+
+// Functionality of the game //
+function getResult() {
+    let result = ""
+    let score = ""
+
+    if (userChoice === compChoice) {
+        // resultDisplay.innerHTML = "It's a Draw!"
+        score = ("<br> Play Score: " + playerScore + "Computer Score " + compScore )
+    } if (
+        (userChoice === 'rock' && compChoice == 'scissors') ||
+        (userChoice === 'scissors' && compChoice == 'paper') ||
+        (userChoice === 'paper' && compChoice == 'rock')
+    ) {
+        // resultDisplay.innerHTML = "You win this round"
+        playerScore++;
+        score = ("Play Score " + playerScore + "Computer Score " + compScore)
+
+        if (playerScore === 5) {
+            result += "You won the match!"
+            document.getElementsByClassName("div.comp-health-bar").style.padding = "10px 5px";
+            disableButton();
+        }
+    } if (
+        (compChoice === 'rock' && userChoice == 'scissors') ||
+        (compChoice === 'scissors' && userChoice == 'paper') ||
+        (compChoice === 'paper' && userChoice == 'rock')
+    ) {
+        // resultDisplay.innerHTML = "Computer takes this round"
+        compScore++;
+        score = ("Play Score: " + playerScore + "Computer Score: " + compScore)
+
+        if (compScore === 5) {
+            result += "You lost the match!"
+            document.getElementsByClassName("div.player-health-bar").style.padding = "10px 5px";
+            disableButton();
+        }
+
+    }
+
+    document.getElementById('result').innerHTML = result;
+}
+
+
+// function to reload the page
+function refreshPage() {
+    window.location.reload();
+}
